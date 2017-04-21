332ea5a4ac27dc0bdefc6793c77d106feb799569
Jayson Isaac <jayson.isaac1@gmail.com>
2017-04-21 09:31:33 -0400

    add m flag

--- myls.c
+++ myls.c
@@ -5,6 +5,7 @@
 #include <stdlib.h> /* for malloc() */
 #include <fcntl.h>  /* for open() */
 #include <dirent.h>
+#include <string.h>
 
 int
 main(int argc, char* argv[]){
@@ -15,22 +16,52 @@ main(int argc, char* argv[]){
   DIR *cdir = opendir(cwd);
   struct dirent *entry;
   if(cdir != NULL){
-    while((entry = readdir(cdir))){
-      if(entry->d_name[0] != '.')
-        printf("%s\t", entry->d_name);
+
+    // No flags
+    if(argc == 1){
+      while((entry = readdir(cdir))){
+        if(entry->d_name[0] != '.')
+          printf("%s\t", entry->d_name);
+      }
+      closedir(cdir);
     }
 
-    closedir(cdir);
+    // -a, Include dir entries that begin with a dot
+    if (strncmp(argv[1],"-a", 2) == 0){
+      while((entry = readdir(cdir))){
+        printf("%s\t", entry->d_name);
+      }
+      closedir(cdir);
+    }
+
+    // -i, Prepend each file's inode number
+    if (strncmp(argv[1], "-i", 2) == 0){
+      while((entry = readdir(cdir))){
+        if(entry->d_name[0] != '.')
+          printf("%llu %s\t", entry->d_ino, entry->d_name);
+      }
+      closedir(cdir);
+    }
+
+    //-m, list files across the page separated by commas
+    if (strncmp(argv[1], "-m", 2) == 0){
+      char s[1024];
+      while((entry = readdir(cdir))){
+        if(entry->d_name[0] != '.'){
+          strcat(s, entry->d_name);
+          strcat(s, ", \t");
+        }
+      }
+      int len = strlen(s);
+      s[len-3] = '\0';
+      printf("%s", s);
+      closedir(cdir);
+    }
+
+    if (strncmp(argv[1], "-l", 2) == 0){
+
+    }
   }
-
-
-  if (argc > 2)
-    printf("error");
-  switch(*argv[1]){
-  }
-
-
-
 
   return 0;
 }
