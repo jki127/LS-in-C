2249e588661607975d0a2a4ce003b067e4f46c9c
Jayson Isaac <jayson.isaac1@gmail.com>
2017-04-25 21:20:37 -0400

    add l flag

--- myls.c
+++ myls.c
@@ -1,11 +1,15 @@
-#include <unistd.h> /* for chdir() */
-#include <stdio.h>  /* for printf() */
-#include <errno.h>  /* for errno */
-#include <string.h> /* for strerror() */
-#include <stdlib.h> /* for malloc() */
-#include <fcntl.h>  /* for open() */
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 int
 main(int argc, char* argv[]){
@@ -18,7 +22,7 @@ main(int argc, char* argv[]){
   if(cdir != NULL){
 
     // No flags
-    if(argc == 1){
+    if(argc < 2){
       while((entry = readdir(cdir))){
         if(entry->d_name[0] != '.')
           printf("%s\t", entry->d_name);
@@ -27,7 +31,7 @@ main(int argc, char* argv[]){
     }
 
     // -a, Include dir entries that begin with a dot
-    if (strncmp(argv[1],"-a", 2) == 0){
+    else if (strncmp(argv[1],"-a", 2) == 0){
       while((entry = readdir(cdir))){
         printf("%s\t", entry->d_name);
       }
@@ -35,7 +39,7 @@ main(int argc, char* argv[]){
     }
 
     // -i, Prepend each file's inode number
-    if (strncmp(argv[1], "-i", 2) == 0){
+    else if (strncmp(argv[1], "-i", 2) == 0){
       while((entry = readdir(cdir))){
         if(entry->d_name[0] != '.')
           printf("%llu %s\t", entry->d_ino, entry->d_name);
@@ -44,7 +48,7 @@ main(int argc, char* argv[]){
     }
 
     //-m, list files across the page separated by commas
-    if (strncmp(argv[1], "-m", 2) == 0){
+    else if (strncmp(argv[1], "-m", 2) == 0){
       char s[1024];
       while((entry = readdir(cdir))){
         if(entry->d_name[0] != '.'){
@@ -58,10 +62,66 @@ main(int argc, char* argv[]){
       closedir(cdir);
     }
 
-    if (strncmp(argv[1], "-l", 2) == 0){
-
+    //-1, one file per line
+    else if (strncmp(argv[1],"-1", 2) == 0){
+      while((entry = readdir(cdir))){
+        printf("%s\t", entry->d_name);
+      }
+      closedir(cdir);
     }
-  }
 
+    // -l:
+    // one file per line with file mode, number of links, owner name, group name,
+    // number of bytes in the file, abbreviated month, day-of-month file was
+    // last modified, hour file last modified, minute file last modified, and
+    // the pathname
+    else if (strncmp(argv[1], "-l", 2) == 0){
+      while((entry = readdir(cdir))){
+        if(entry->d_name[0] != '.'){
+          struct stat f_info;
+          int i = stat(entry->d_name, &f_info);
+
+          // Permissions
+          printf( (S_ISDIR(f_info.st_mode)) ? "d" : "-");
+          printf( (f_info.st_mode & S_IRUSR) ? "r" : "-");
+          printf( (f_info.st_mode & S_IWUSR) ? "w" : "-");
+          printf( (f_info.st_mode & S_IXUSR) ? "x" : "-");
+          printf( (f_info.st_mode & S_IRGRP) ? "r" : "-");
+          printf( (f_info.st_mode & S_IWGRP) ? "w" : "-");
+          printf( (f_info.st_mode & S_IXGRP) ? "x" : "-");
+          printf( (f_info.st_mode & S_IROTH) ? "r" : "-");
+          printf( (f_info.st_mode & S_IWOTH) ? "w" : "-");
+          printf( (f_info.st_mode & S_IXOTH) ? "x" : "-");
+
+          // number of hard links
+          printf("  %d", f_info.st_nlink);
+
+          // user name
+          struct passwd *pws;
+          pws = getpwuid(f_info.st_uid);
+          printf(" %s", pws->pw_name);
+
+          // group name
+          struct group *grp;
+          grp = getgrgid(f_info.st_gid);
+          printf("  %s", grp->gr_name);
+
+          // file size in bytes
+          printf("  %llu", f_info.st_size);
+
+          // last modification date
+          struct tm *modif_time;
+          char time_buf[20];
+          modif_time = localtime(&f_info.st_mtime);
+          strftime(time_buf, sizeof(time_buf), "%b %d %H:%M", modif_time);
+          printf(" %s", time_buf);
+
+          // file name
+          printf(" %s\n", entry->d_name);
+        }
+      }
+      closedir(cdir);
+    }
   return 0;
+  }
 }
